46a8da8d27c5669e74186e658d979be3a1c64e3b
Marcos Caballero <marcowerjr@gmail.com>
2021-03-17 09:01:28 -0700

    Update files

--- lib/src/MyApp.dart
+++ lib/src/MyApp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:listview/routes.dart';
 import 'package:listview/src/pages/ErrorPage.dart';
  
@@ -15,6 +16,16 @@ class MyApp extends StatelessWidget {
       onGenerateRoute: (RouteSettings settings) => MaterialPageRoute(
         builder: (BuildContext context) => ErrorPage()
       ),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
+      supportedLocales: [
+        const Locale('en', ''),
+        const Locale('es', 'ES'), //Español de España
+        const Locale('es', 'MX')
+      ],
     );
   }
 }
--- lib/src/pages/InputsPage.dart
+++ lib/src/pages/InputsPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class InputsPage extends StatefulWidget {
   @override
@@ -7,13 +8,15 @@ class InputsPage extends StatefulWidget {
 
 class _InputsPageState extends State<InputsPage> {
   String _name = '', _email = '', _password = '';
+  //Control para manipular el input
+  TextEditingController _input = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return _body();
+    return _body(context);
   }
 
-  Widget _body() {
+  Widget _body(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Página de Inputs'),
@@ -24,11 +27,11 @@ class _InputsPageState extends State<InputsPage> {
         },
         child: Icon(Icons.arrow_back),
       ),
-      body: _inputElements(),
+      body: _inputElements(context),
     );
   }
 
-  Widget _inputElements() => ListView(
+  Widget _inputElements(BuildContext context) => ListView(
         padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
         children: <Widget>[
           _textField(),
@@ -36,6 +39,8 @@ class _InputsPageState extends State<InputsPage> {
           _emailField(),
           Divider(),
           _passwordField(),
+          Divider(),
+          _dateField(context),
           Divider(),
           _showData()
         ],
@@ -46,8 +51,8 @@ class _InputsPageState extends State<InputsPage> {
         textCapitalization: TextCapitalization.sentences, //teclado
         decoration: InputDecoration(
             border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-            hintText: 'Name:',
-            labelText: 'Name',
+            hintText: 'Nombre:',
+            labelText: 'Nombre',
             helperText: 'Aquí va tu nombre *_*',
             suffixIcon: Icon(Icons.text_fields), //aparece a la derecha de field
             icon: Icon(Icons.account_circle),
@@ -76,14 +81,14 @@ class _InputsPageState extends State<InputsPage> {
             _email = text;
           });
         },
-  );
+      );
   Widget _passwordField() => TextField(
         //textCapitalization: TextCapitalization.sentences, //teclado
         obscureText: true,
         decoration: InputDecoration(
             border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-            hintText: 'Password:',
-            labelText: 'Password',
+            hintText: 'Contraseña:',
+            labelText: 'Contraseña',
             helperText: 'Aquí va tu contraseña *_*',
             suffixIcon: Icon(Icons.security), //aparece a la derecha de field
             icon: Icon(Icons.lock),
@@ -94,7 +99,50 @@ class _InputsPageState extends State<InputsPage> {
             _password = text;
           });
         },
-  );
+      );
+
+  Widget _dateField(BuildContext context) => TextField(
+        controller: _input,
+        enableInteractiveSelection: false,
+        decoration: InputDecoration(
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+            hintText: 'Fecha de nacimiento',
+            labelText: 'Fecha de nacimiento',
+            helperText: 'Campo modal',
+            suffixIcon:
+                Icon(Icons.calendar_today), //aparece a la derecha de field
+            icon: Icon(Icons.date_range),
+            counter: Text('Caracteres: ${_password.length}')),
+        onTap: () {
+          FocusScope.of(context).requestFocus(FocusNode());
+          // _getDate(context);
+          _getDate(context);
+        },
+      );
+
+  Future<void> _getDate(BuildContext context) async {
+    DateTime fecha = await showDatePicker(
+        context: context,
+        locale: Locale('es', 'MX'),
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2015),
+        lastDate: DateTime(2030));
+    if (fecha != null) {
+      _input.text = fecha.toLocal().toUtc().toString();
+    }
+  }
+
+  Future<void> _toast() async {
+    Fluttertoast.showToast(
+        msg: "This is Center Short Toast",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
+
   Widget _showData() => ListTile(
         title: Text('Name: $_name\nCorreo: $_email\nPassword: $_password'),
       );
